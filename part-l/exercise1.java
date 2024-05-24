910f858aa19985fdee35cabd3948d95cb1820e96
Arretados Labs <arretadolabs@gmail.com>
2024-05-23 21:29:54 -0300

    first commit exercise 01

---
+++ part-l/exercise1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args) {
+
+        //Importing class Scanner for input data 
+        Scanner sc = new Scanner(System.in);
+
+        //Section variables
+        String name;
+
+        //Input data user
+        System.out.println("Input a name: ");
+        name = sc.nextLine();
+
+        // Printing result final
+        System.out.println("Hello \n" + name);
+
+        // Closing class Scanner
+        sc.close();
+
+    }
+}
