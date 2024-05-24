74de5791f994b6e8b0e6bafcc7dd7b6156d21e6b
Arretados Labs <arretadolabs@gmail.com>
2024-05-23 21:51:05 -0300

    exercise 03 - divide two numbers

---
+++ part-l/exercise03.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class exercise03 {
+    public static void main(String[] args) {
+        /**
+         * Write a Java program to divide two numbers and print them on the screen.
+         * Test Data :
+         * 50/3
+         * Expected Output :
+         * 16
+         */
+
+        // Importing class Scanner for input data user
+        Scanner sc = new Scanner(System.in);
+
+        // Section variables
+        int numberOne;
+        int numberTwo;
+
+        // Input data user
+        System.out.println("Number one: ");
+        numberOne = sc.nextInt();
+
+        System.out.println("Number two: ");
+        numberTwo = sc.nextInt();
+
+        // Printing value final
+        System.out.println("Result final = " + numberOne / numberTwo);
+
+    }
+}
