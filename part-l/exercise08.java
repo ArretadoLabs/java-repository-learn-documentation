999df6882a20cf499cd30bbb47068ee69b9d22d5
Arretados Labs <arretadolabs@gmail.com>
2024-05-23 22:47:43 -0300

    formatter lines comments

--- part-l/exercise08.java
+++ part-l/exercise08.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class exercise08 {
     public static void main(String[] args) {
-        //Importing class Scanner for input data user
+        // Importing class Scanner for input data user
         Scanner sc = new Scanner(System.in);
 
-        //Section of variables
+        // Section of variables
         int numberOne;
         int numberTwo;
         int numberThree;
         int averageNumbers;
 
-        //Input data user
+        // Input data user
         System.out.println("Number one: ");
         numberOne = sc.nextInt();
 
@@ -20,14 +20,14 @@ public class exercise08 {
 
         System.out.println("Number three: ");
         numberThree = sc.nextInt();
-        
-        //Result final of average three numbers
+
+        // Result final of average three numbers
         averageNumbers = (numberOne + numberTwo + numberThree) / 3;
 
-        //Printing value final of average
+        // Printing value final of average
         System.out.println("Result final of average: " + averageNumbers);
 
-        //Closing class Scanner
+        // Closing class Scanner
         sc.close();
     }
 }
