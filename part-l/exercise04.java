62b69cdb668e02545ca66cc8c5cba14a9790e48f
Arretados Labs <arretadolabs@gmail.com>
2024-05-23 21:58:23 -0300

    exercise 04 - operations math results

---
+++ part-l/exercise04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercise04 {
+    public static void main(String[] args) {
+        // Importing class Scanner input data user
+        Scanner sc = new Scanner(System.in);
+
+        // Section of printing values operations mathematicals
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (-3) * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+        // Closing class Scanner
+        sc.close();
+
+    }
+}
