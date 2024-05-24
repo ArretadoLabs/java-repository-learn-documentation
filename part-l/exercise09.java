a20dc17677e11226c7902280ad6e8084fe54ea8e
Arretados Labs <arretadolabs@gmail.com>
2024-05-23 23:06:33 -0300

    exercise 09 - calculating area and perimeter of rectangle

---
+++ part-l/exercise09.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercise09 {
+    public static void main(String[] args) {
+        // Importing class Scanner for data user input
+        Scanner sc = new Scanner(System.in);
+
+        // Section variables
+        double width;
+        double height;
+
+        // Input data user
+        System.out.println("Width: ");
+        width = sc.nextDouble();
+
+        System.out.println("height: ");
+        height = sc.nextDouble();
+
+        // Calculating operation logical
+        System.out.println("The area of rectangle is: " + (width * height));
+        System.out.println("The perimeter of rectangle is: " + (2 * (width + height)));
+
+        //Closing class Scanner
+        sc.close();
+
+    }
+}
