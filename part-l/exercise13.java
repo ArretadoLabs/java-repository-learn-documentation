6a21567d22f8d82b5cb29ef4886bf3fe656af6d2
Arretados Labs <arretadolabs@gmail.com>
2024-05-28 21:24:01 -0300

    exercise for compare value of two numbers

---
+++ part-l/exercise13.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class exercise13 {
+    public static void main(String[] args) {
+        /**
+         * Importing class Scanner for input data user
+         */
+        Scanner sc = new Scanner(System.in);
+
+        // Variables
+        int numberOne;
+        int numberTwo;
+
+        // Input data user
+        System.out.println("Number one: ");
+        numberOne = sc.nextInt();
+
+        System.out.println("Number two: ");
+        numberTwo = sc.nextInt();
+
+        // Structure logical for compare two numbers
+        if (numberOne > numberTwo) {
+            System.out.println(numberOne + " > " + numberTwo);
+        } else if (numberOne < numberTwo) {
+            System.out.println(numberOne + " < " + numberTwo);
+        } else {
+            System.out.println("Two numbers is equal " + numberOne + " = " + numberTwo);
+        }
+
+    }
+}
