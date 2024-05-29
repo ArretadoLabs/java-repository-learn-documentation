a3a14034a7b0e8ed80be0a32ea9bd1c5646fed49
Arretados Labs <arretadolabs@gmail.com>
2024-05-28 23:04:00 -0300

    printing value logical if third number is equal sum of two numbers

---
+++ part-l/exercise20.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class exercise20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Variables
+        int numberOne;
+        int numberTwo;
+        int numberThree;
+        int sumNumbers;
+
+        // input data user
+        System.out.println("number one: ");
+        numberOne = sc.nextInt();
+
+        System.out.println("number two: ");
+        numberTwo = sc.nextInt();
+
+        System.out.println("number three: ");
+        numberThree = sc.nextInt();
+
+        // Sum of numbers
+        sumNumbers = numberOne + numberTwo;
+
+        // Creating structure logical
+        if (numberThree == sumNumbers) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        // Closing class Scanner
+        sc.close();
+    }
+}
