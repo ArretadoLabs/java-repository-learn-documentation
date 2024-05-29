3563407ab7fd081447c1a8fa6f6f40d6c4797db8
Arretados Labs <arretadolabs@gmail.com>
2024-05-28 22:51:23 -0300

    printing numbers odd in between 1 to 99

---
+++ part-l/exercise18.java
@@ -0,0 +1,12 @@
+public class exercise18 {
+    public static void main(String[] args) {
+        /**
+         * Printing numbers odd in between 1 to 99
+         */
+        for (int i = 0; i < 101; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);                
+            }
+        }
+    }
+}
