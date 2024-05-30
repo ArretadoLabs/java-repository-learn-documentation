d6e2d06e52f8aeec1a4c68d366edff572a2d4635
Arretados Labs <arretadolabs@gmail.com>
2024-05-29 23:44:45 -0300

    input month and display number of days

---
+++ Conditional Statement/Exercise4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        // Importing class Scanner for input data user
+        Scanner sc = new Scanner(System.in);
+
+        // variables
+        int numberMonth, numberYear;
+
+        // Input data user
+        System.out.println("Input a number of month: ");
+        numberMonth = sc.nextInt();
+
+        System.out.println("Input a number of year: ");
+        numberYear = sc.nextInt();
+
+        // Structure logical conditional
+        if (numberMonth == 2) {
+            System.out.println("28 days");
+            System.out.println(numberYear);
+        } else if (numberMonth == 4 || numberMonth == 6 || numberMonth == 10) {
+            System.out.println("30 days");
+            System.out.println(numberYear);
+        } else {
+            System.out.println("31 days");
+            System.out.println(numberYear);
+        }
+
+        //Closing class Scanner
+        sc.close();
+    }
+}
