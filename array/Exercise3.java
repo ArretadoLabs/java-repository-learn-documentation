b8aa105e053e3bb90b252b62488bb431eba95815
Arretados Labs <arretadolabs@gmail.com>
2024-06-03 10:22:21 -0300

    Adjustment code for closing class Scanner

--- array/Exercise3.java
+++ array/Exercise3.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Exercise3 {
     public static void main(String[] args) {
 
+        // Importing class Scanner for data user input
+        Scanner input = new Scanner(System.in);
+
         // Creating vector and variables
         int[] numbers = new int[5];
         int sum = 0;
 
         // Structure logical for input and calculate value
         for (int i = 0; i < 5; i++) {
-            Scanner input = new Scanner(System.in);
             System.out.printf("Number of vector[%d] :", i);
             numbers[i] = input.nextInt();
         }
@@ -20,6 +22,9 @@ public class Exercise3 {
             sum += i;
         }
         System.out.println("Sum of values in vector is: " + sum);
+
+        //Closing class Scanner
+        input.close();
 
     }
 }
