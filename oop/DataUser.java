89a9a27229f4d19c9b0aa1c30966c3edb1273760
Arretados Labs <arretadolabs@gmail.com>
2024-06-05 11:47:33 -0300

    OOP display data user in development

---
+++ oop/DataUser.java
@@ -0,0 +1,57 @@
+package oop;
+
+import java.util.Scanner;
+
+public class DataUser {
+    public static void main(String[] args) {
+
+        People people = new People();
+        people.ReadName();
+        people.ReadCPF();
+        people.ReadAge();
+        people.DisplayData();
+
+    }
+}
+
+// Class People
+class People {
+    // Attributes
+    private String name;
+    private String cpf;
+    private int age;
+
+    // Creating methods
+    public void ReadName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What your name: ");
+        name = input.nextLine();
+
+        // Closing class Scanner in scope local
+        input.close();
+    }
+
+    public void ReadCPF() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What your CPF: ");
+        String cpf = input.nextLine();
+        input.close();
+
+    }
+
+    public void ReadAge() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What you age: ");
+        age = input.nextInt();
+
+        // Closing class Scanner in scope local
+        input.close();
+    }
+
+    public void DisplayData() {
+        System.out.println(name);
+        System.out.println(cpf);
+        System.out.println(age);
+    }
+
+}
