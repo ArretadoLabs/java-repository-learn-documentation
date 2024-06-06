ad4ece11e7957695305c6fbc8dc04e070443ed4d
Arretados Labs <arretadolabs@gmail.com>
2024-06-06 09:55:03 -0300

    Display data user with validation

---
+++ oop/DataUserValidation.java
@@ -0,0 +1,69 @@
+package oop;
+
+import java.util.Scanner;
+
+public class DataUserValidation {
+    public static void main(String[] args) {
+
+    }
+}
+
+class peopleData {
+    private String name;
+    private String cpf;
+    private int age;
+    private String numberPhone;
+
+    // Section about methods
+    public void readName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your name is: ");
+        name = input.nextLine();
+        if (name.length() < 3) {
+            System.out.println("name invalid, try again");
+            System.out.println("Input your name: ");
+            name = input.nextLine();
+        }
+    }
+
+    public void readCpf() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your name is: ");
+        cpf = input.nextLine();
+        if (cpf.length() != 11) {
+            System.out.println("Data CPF is invalid, try again");
+            System.out.println("You CPF: ");
+            cpf = input.nextLine();
+        }
+    }
+
+    public void readAge() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What you age, please: ");
+        age = input.nextInt();
+        if (age <= 0 || age > 150) {
+            System.out.println("You age is invalid, try again");
+            System.out.println("Your age please again: ");
+            age = input.nextInt();
+        }
+    }
+
+    public void readNumberPhone() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("You cellphone: ");
+        numberPhone = input.nextLine();
+        if (numberPhone.length() != 11) {
+            System.out.println("Size numbers is invalid, try again");
+            System.out.println("Your number phone is: ");
+            numberPhone = input.nextLine();
+        }
+    }
+
+    public void displayData() {
+        System.out.println(name);
+        System.out.println(cpf);
+        System.out.println(age);
+        System.out.println(numberPhone);
+    }
+
+}
